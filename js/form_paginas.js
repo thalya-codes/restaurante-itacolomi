adc25a34033332fe60c6f179b9e47c54ab2738e0
Thalya Stéffany <thalyacoiimbra@gmail.com>
2022-06-20 08:58:31 -0300

    .

--- js/form_paginas.js
+++ js/form_paginas.js
@@ -8,33 +8,52 @@ const sSecond = document.querySelector(".form__second-step")
 const sFirst = document.querySelector(".form__first-step")
 const imgSeta = document.querySelector("[data-img-seta]")
 
-const validaCampos = _ => {
+const validaCampos = e => {
     const quantEncontrada = quantPessoas.find(quantP => quantP.checked === true)
     const dispoEncontrada = disponibilidade.find(dispo => dispo.checked === true)
+    const inputsText =  $All(".form__input-text")
+    const selectCategoria = document.querySelector(".form__input-select--categoria")
+    const options = [...document.querySelectorAll('option')]
 
+    selectCategoria.addEventListener("change", (e, indx) => {
+        console.log(e.target)
+    })
+    /*  convertendo value para string ++ condicional
+    const str = options[0].value.toString()
+    console.log(str.indexOf("parraa"))
+    
+    */
     if(quantEncontrada && dispoEncontrada) {
         btnProximaPag.removeAttribute("disabled")
-    } 
+        return true
+    }  else {
+        return false
+    }
 }
 
 //visibilidade das páginas
+btnProximaPag.onclick = e => {
+    e.preventDefault()
+}
+/*
 btnProximaPag.addEventListener("click", e => {
     e.preventDefault()
-    sFirst.style.display = "none" 
-    btnProximaPag.style.display = "none" 
+
+
+    if(validaCampos()) {
+        sFirst.style.display = "none" 
+        btnProximaPag.style.display = "none" 
+        
+        sSecond.style.display = "flex" 
+        btnCadastrar.style.display = "block" 
+    }
     
-    sSecond.style.display = "flex" 
-    btnCadastrar.style.display = "block" 
 
 })
+*/
+
 //voltar etapas
-imgSeta.addEventListener("click", _ => {    
-    sFirst.style.display = "flex"
-    sSecond.style.display = "none"
-    btnCadastrar.style.display = "none" 
-    btnProximaPag.style.display = "block" 
 
-})
 //escuta clicks no form
 form.addEventListener("click", validaCampos)
 
