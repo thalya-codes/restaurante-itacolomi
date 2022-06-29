982ebe3fbbb7791b38f2b83f5ed1af083466209f
Thalya Stéffany <thalyacoiimbra@gmail.com>
2022-06-29 09:56:46 -0300

    Ajustes na função DisplayRegisteredProducts e no estilos de seus filhos

--- data.js
+++ data.js
@@ -11,10 +11,10 @@ const products = [
     },
     {
         id:  1,
-        img: urlImg, title: "Salada de polvo",
-        amount: "Serve 1 pessoa",
-        description: "Polvo fresco com legumes refogados (Cenoura, cebola, pimentão, grão de bico e pepino)...",
-        price: "100.00",
+        img: urlImg, title: "Salada de ovo",
+        amount: "Serve 2 pessoa",
+        description: "OVO fresco com legumes refogados (Cenoura, cebola, pimentão, grão de bico e pepino)...",
+        price: "10.00",
     },
     
 ]
--- js/components/Resgistered_Products/Resgistered_Products.js
+++ js/components/Resgistered_Products/Resgistered_Products.js
@@ -2,115 +2,49 @@ import HasRegisteredProducts from "../../validations/has_registered_products.js"
 import products from "../../../data.js";
 const main = document.querySelector("main")
 
-const $Create = document.createElement.bind(document)
 
 export const DisplayRegisteredProducts = () => {
-    const btnResgisterMore = $Create("button")
+    const btnResgisterMore = document.createElement("button")
+    btnResgisterMore.classList.add("preview-products__btn-register-more",  "msg-no-product__button")
+    btnResgisterMore.innerText = "Cadastrar mais produtos"
 
-    products.map(product => {
-        //create 
-        const divPreviewProdutcs = $Create("div")
-        const divContainer = $Create("div")
-        const img = $Create("img")
-        const divContainerDescription = $Create("div")
-        const descriptionTitle = $Create("h3")
-        const descriptionAmount =  $Create("span")
-        const descriptionP =  $Create("p")
-        const descriptionPrice =  $Create("span")
-        const btnContainer =  $Create("div")
-        const btnEdit = $Create("button")
-        const btnDelete = $Create("button")
+    main.innerHTML = `
+        ${products.map(product => (
+            `
+            <div class="preview-products" id=${product.id}>
+                <div class="d-flex" data-container-preview-products>
+                    <img src=${product.img} alt=""
+                    class="preview-products__img preview-products__items"
+                    data-img-product
+                    >
 
+                    <div 
+                    class="preview-products__container-description
+                    preview-products__item d-flex f-direction-column 
+                    justify-space-between " data-container-description>
 
+                        <h3 class="preview-products__title" data-title-product>${product.title}</h3>
+                        <span class="preview-products__amount"data-amount>${product.amount}</span>
+                        <p class="preview-products__description justify-space-between" data-description>
+                        ${product.description}
+                        </p>
+                        <span class="preview-products__prince" data-prince>R$ ${product.price}</span>
+                    </div>
 
-        //add style
-        divPreviewProdutcs.classList.add("preview-products")
-        
-        divContainer.classList.add("d-flex")
-
-        img.classList.add(
-        "preview-products__img",
-        "preview-products__items"
-        )
-
-        divContainerDescription.classList.add(
-        "preview-products__container-description",
-        "preview-products__item",
-        "d-flex",
-        "f-direction-column",
-        "justify-space-between"
-        )
-        
-        descriptionTitle.classList.add("preview-products__title")
-
-        descriptionAmount.classList.add("preview-products__amount")
-
-        descriptionP.classList.add(
-        "preview-products__description",
-        "justify-space-between"
-        )
-
-        descriptionPrice.classList.add("preview-products__prince")
-
-        btnContainer.classList.add(
-        "preview-products__btn-container",
-        "preview-products__item",
-        "d-flex",
-        "justify-space-between"
-        )
-
-        btnEdit.classList.add("preview-products__btn", "edit")
-        
-        btnDelete.classList.add("preview-products__btn", "delete")
-        
-        btnResgisterMore.classList.add(
-        "preview-products__btn-register-more",
-        "msg-no-product__button"
-        )
-
-
-
-        //add content
-        img.src= product.img
-        descriptionTitle.innerHTML = product.title
-        descriptionAmount.innerHTML = product.amount
-        descriptionP.innerHTML = product.description
-        descriptionPrice.innerHTML = product.price
-        btnEdit.innerHTML = "Editar"
-        btnDelete.innerHTML = "Excluir"
-        btnResgisterMore.innerHTML = "Cadastrar mais produtos"
-        
- 
-        //append 
-        divPreviewProdutcs.append(
-            divContainer,
-            btnResgisterMore
-        )
-            
-        divContainer.append(
-            img,
-            divContainerDescription,
-            btnContainer
-        )
-
-        divContainerDescription.append(
-            descriptionTitle,
-            descriptionAmount,
-            descriptionP,
-            descriptionPrice
-        )
-
-        btnContainer.append(
-            btnEdit,
-            btnDelete
-        )
-
-        main.append(divPreviewProdutcs, btnResgisterMore)
+                    <div class="preview-products__btn-container preview-products__item d-flex justify-space-between">
+                        <button class="preview-products__btn editar" data-edit>Editar</button>
+                        <button class="preview-products__btn" data-delete>Excluir</button>
+                    </div>
+                </div>
+           </div>
+            `
+        ))}
     
-    })
+    `
+
+    main.insertAdjacentElement("beforeend", btnResgisterMore)
 
 }
-
 
 
 HasRegisteredProducts(products)
